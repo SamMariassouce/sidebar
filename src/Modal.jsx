bee01eb28f8a64d84e66699c9b126d9026864d0c
Sam Mariassouce <sammariassouce@gmail.com>
2023-06-14 16:47:27 -1000

    second

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,4 +1,15 @@
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+
 const Modal = () => {
-  return <div>Modal</div>;
+  const { isModal, closeModal } = useGlobalContext();
+  return (
+    <div className="modal-overlay">
+      <h3>modal content</h3>
+      <button className="close-modal-btn" onClick={closeModal}>
+        <FaTimes />
+      </button>
+    </div>
+  );
 };
 export default Modal;
